5e7eb88b3988e76223f2a6a36f85699d5410b3d1
無常 <3142324836@qq.com>
2026-09-08 00:47:54 +0800

    Add wasm release ci

--- scripts/ci/matrix/genReleaseMatrix.ps1
+++ scripts/ci/matrix/genReleaseMatrix.ps1
@@ -1,14 +1,15 @@
 param(
-    [Parameter(Mandatory = $true)] [string] $EnableAndroidArm64,
-    [Parameter(Mandatory = $true)] [string] $EnableAndroidX64,
-    [Parameter(Mandatory = $true)] [string] $EnableIosArm64,
-    [Parameter(Mandatory = $true)] [string] $EnableIossimuArm64,
-    [Parameter(Mandatory = $true)] [string] $EnableWindowsX64,
-    [Parameter(Mandatory = $true)] [string] $EnableWindowsArm64,
-    [Parameter(Mandatory = $true)] [string] $EnableMacosArm64,
-    [Parameter(Mandatory = $true)] [string] $EnableMacosX64,
-    [Parameter(Mandatory = $true)] [string] $EnableLinuxX64,
-    [Parameter(Mandatory = $true)] [string] $EnableLinuxArm64
+    [Parameter(Mandatory = $false)] [string] $EnableAndroidArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableAndroidX64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableIosArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableIossimuArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableWindowsX64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableWindowsArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableMacosArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableMacosX64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableLinuxX64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableLinuxArm64 = 'false',
+    [Parameter(Mandatory = $false)] [string] $EnableWasm = 'false'
 )
 
 $enabled = @{
@@ -22,6 +23,7 @@ $enabled = @{
     'macos-x64'      = $EnableMacosX64      -eq 'true'
     'linux-x64'      = $EnableLinuxX64      -eq 'true'
     'linux-arm64'    = $EnableLinuxArm64    -eq 'true'
+    'wasm'           = $EnableWasm          -eq 'true'
 }
 $androidEnabled = $enabled['android-arm64'] -or $enabled['android-x64']
 $iosEnabled     = $enabled['ios-arm64']     -or $enabled['iossimu-arm64']
@@ -44,8 +46,10 @@ $releaseAll = @(
     @{ target='Linux';   arch='x64';   os='ubuntu-latest';    'build-type'='release'; key='linux-x64' }
     @{ target='Linux';   arch='arm64'; os='ubuntu-24.04-arm'; 'build-type'='debug';   key='linux-arm64' }
     @{ target='Linux';   arch='arm64'; os='ubuntu-24.04-arm'; 'build-type'='release'; key='linux-arm64' }
+    @{ target='WASM';    arch='wasm32'; os='windows-latest';   'build-type'='debug';   key='wasm' }
+    @{ target='WASM';    arch='wasm32'; os='windows-latest';   'build-type'='release'; key='wasm' }
 )
-$release = $releaseAll | Where-Object {
+$release = @($releaseAll | Where-Object {
     ($_.key -ne 'android'      -or $androidEnabled) -and
     ($_.key -ne 'ios-arm64'    -or $iosEnabled) -and
     ($_.key -ne 'windows-x64'  -or $enabled['windows-x64']) -and
@@ -53,10 +57,11 @@ $release = $releaseAll | Where-Object {
     ($_.key -ne 'macos-arm64'  -or $enabled['macos-arm64']) -and
     ($_.key -ne 'macos-x64'    -or $enabled['macos-x64']) -and
     ($_.key -ne 'linux-x64'    -or $enabled['linux-x64']) -and
-    ($_.key -ne 'linux-arm64'  -or $enabled['linux-arm64'])
-} | ForEach-Object { $_.Remove('key'); $_ }
+    ($_.key -ne 'linux-arm64'  -or $enabled['linux-arm64']) -and
+    ($_.key -ne 'wasm'         -or $enabled['wasm'])
+} | ForEach-Object { $_.Remove('key'); $_ })
 
-$json = ($release | ConvertTo-Json -Compress -Depth 5)
-if ($release.Count -eq 1) { $json = "[$json]" }
+# Ensure ConvertTo-Json receives the array as a whole
+$json = ConvertTo-Json -InputObject $release -Compress -Depth 5
 
 Write-Output $json
