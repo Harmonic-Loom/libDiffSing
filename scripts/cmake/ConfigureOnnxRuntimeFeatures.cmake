396da154f23e25bfbd0a2dd542b59efcab9445a0
無常 <3142324836@qq.com>
2026-09-09 03:21:44 +0800

    Removed winml

--- scripts/cmake/ConfigureOnnxRuntimeFeatures.cmake
+++ scripts/cmake/ConfigureOnnxRuntimeFeatures.cmake
@@ -10,10 +10,10 @@ set(ONNXRUNTIME_FEATURES "")
 set(ONNXRUNTIME_FEATURES_JSON "")
 
 if(VCPKG_TARGET_TRIPLET MATCHES "^x64-windows")
-    set(ONNXRUNTIME_FEATURES "cuda" "openvino" "tensorrt" "directml" "winml" "xnnpack")
+    set(ONNXRUNTIME_FEATURES "cuda" "openvino" "tensorrt" "directml" "xnnpack")
 
 elseif(VCPKG_TARGET_TRIPLET MATCHES "^arm64-windows")
-    set(ONNXRUNTIME_FEATURES "kleidiai" "directml" "winml")
+    set(ONNXRUNTIME_FEATURES "kleidiai" "directml")
 
 elseif(VCPKG_TARGET_TRIPLET MATCHES "^x64-linux")
     set(ONNXRUNTIME_FEATURES "cuda" "tensorrt" "xnnpack")
